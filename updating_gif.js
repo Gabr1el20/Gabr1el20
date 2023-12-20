2f2ba2f587b6dc75385960b96236fb795c20ed76
Gabriel Delgado <6327@holbertonstudents.com>
2023-12-20 20:31:58 +0000

    Improving workflow, adding new regex to display more info!

--- updating_gif.js
+++ updating_gif.js
@@ -23,15 +23,29 @@ function getImageSrcFromContent (content) {
   return match ? match[1] : null
 }
 
+function getAnimatorFromContent (content) {
+  const regex = /Key animation: -(.*?)-/ // Expresión regular para buscar el valor después de Key animation:
+  const match = content.match(regex)
+  return match ? match[1] : null
+}
+
+function getSeriesFromContent (content) {
+  const regex = /From -(.*?)-/ // Expresión regular para buscar el valor después de Series:
+  const match = content.match(regex)
+  return match ? match[1] : null
+}
+
 // Función para actualizar el archivo TEST.md con el nuevo valor de src
-async function updateTestFile (imageSrc) {
+async function updateTestFile (imageSrc, animator, series) {
   try {
     const testFilePath = 'README.md' // Reemplaza 'RUTA_A_TEST.md' con la ruta real de tu archivo TEST.md
     let testFileContent = fs.readFileSync(testFilePath, 'utf-8')
 
     const urlToChange = getImageSrcFromContent(testFileContent)
+    const KAToChange = getAnimatorFromContent(testFileContent)
+    const SeriesToChange = getSeriesFromContent(testFileContent)
     // Reemplazar {{IMAGE_SRC}} con el nuevo valor
-    testFileContent = testFileContent.replace(urlToChange, imageSrc)
+    testFileContent = testFileContent.replace(urlToChange, imageSrc).replace(KAToChange, animator).replace(SeriesToChange, series)
 
     // Guardar el archivo actualizado
     fs.writeFileSync(testFilePath, testFileContent, 'utf-8')
@@ -48,9 +62,11 @@ async function main () {
   try {
     const apiData = await fetchDataFromAPI()
     const imageSrc = apiData.url
+    const keyAnimator = apiData['author(s)']
+    const source = `${apiData.series}: ${apiData.source}`
 
     // Actualizar el archivo TEST.md con el nuevo valor de src
-    await updateTestFile(imageSrc)
+    await updateTestFile(imageSrc, keyAnimator, source)
   } catch (error) {
     console.error('Error en el script principal:', error)
   }
